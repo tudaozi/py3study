08b1e737ee6620431154f5ca37b7260fcf29ed34
Tudaozi <1626202029@qq.com>
2019-03-22 23:50:35 +0800

    学习课程更新

---
+++ c01_14_summary.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: Tudaozi
+@contact: tudaozi@faisen.com
+@project: py3study
+@file: c01_14_summary.py
+@time: 2019-03-22 23:26
+@desc:
+'''
+
+spam ="7"
+spam=spam+"0"
+eggs=int(spam)+3
+print(float(eggs))
+
+
+word =input("Enter a word:")
+print(word+'shop')
+
+x=5
+y=x+3
+y=int(str(y)+"2")
+print(y)
+
+x=5
+x+=5
+print(x)
+
+x=3
+num=14
+print(num%x)
