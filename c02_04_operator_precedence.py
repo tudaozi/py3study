a87795aa984667561efcb3320d1ea05ad7abb15b
Tudaozi <46038475+tudaozi@users.noreply.github.com>
2019-03-26 19:42:03 +0800

    练习

---
+++ c02_04_operator_precedence.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding=UTF-8
+'''
+@Description: operator precedence
+@Author: Tudaozi
+@WebSite: https://www.staurl.com
+@LastEditors: Tudaozi
+@Date: 2019-03-26 11:18:57
+@LastEditTime: 2019-03-26 13:44:13
+'''
+x = 4
+y = 2
+if not 1+1 == y or x == 4 and 7 == 8:
+    print("yes")
+elif x > y:
+    print("no")
