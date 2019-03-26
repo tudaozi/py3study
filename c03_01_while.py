c9e70368c3e2b2877cfdc230be129f32345b3bdb
Tudaozi <46038475+tudaozi@users.noreply.github.com>
2019-03-26 23:00:56 +0800

    练习

--- c03_01_while.py
+++ c03_01_while.py
@@ -6,5 +6,20 @@
 @WebSite: https://www.staurl.com
 @LastEditors: Tudaozi
 @Date: 2019-03-25 13:03:36
-@LastEditTime: 2019-03-25 13:28:32
+@LastEditTime: 2019-03-26 22:41:55
 '''
+i = 0
+while 1 == 1:
+    print(i)
+    i += 1
+    if i >= 5:
+        print("Breaking")
+        break
+print("Finished")
+
+i = 5
+while True:
+    print(i)
+    i -= 1
+    if i <= 2:
+        break
