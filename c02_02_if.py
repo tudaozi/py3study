ae5866130c767369fad6c1e2c0edf7b214e9173a
Tudaozi <46038475+tudaozi@users.noreply.github.com>
2019-03-26 11:31:16 +0800

    练习

---
+++ c02_02_if.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding=UTF-8
+'''
+@Description: 
+@Author: Tudaozi
+@WebSite: https://www.staurl.com
+@LastEditors: Tudaozi
+@Date: 2019-03-26 11:18:57
+@LastEditTime: 2019-03-26 11:30:43
+'''
+if 10 > 5:
+    print("10 大于 5")
+print("Program ended")
+
+spam = 7
+if spam > 5:
+    print("five")
+if spam > 8:
+    print("eight")
